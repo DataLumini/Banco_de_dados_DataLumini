d8ce3ee5ecf3d09a3fd99d8fed7df279f48d55ab
liviabritomotta <livia.motta@sptech.school>
2026-04-07 10:36:47 -0300

    adicionando banco de dados

---
+++ bancode dadosDataLumini.sql
@@ -0,0 +1,89 @@
+-- banco de dados datalumini
+
+CREATE DATABASE datalumini;
+
+USE datalumini;
+
+
+
+CREATE TABLE empresa (
+idempresa INT PRIMARY KEY AUTO_INCREMENT,
+razaosocial VARCHAR(255),
+cnpj CHAR(11),
+email VARCHAR(255),
+responsavel_legal VARCHAR(255),
+status_empresa TINYINT);
+
+CREATE TABLE usuario(
+idusuario INT PRIMARY KEY AUTO_INCREMENT,
+email VARCHAR(255),
+senha VARCHAR(255),
+cpf CHAR(11),
+fkempresa INT, 
+CONSTRAINT chfkempresausuario
+FOREIGN KEY (fkempresa) REFERENCES empresa (idempresa));
+
+CREATE TABLE estufa (
+idestufa INT PRIMARY KEY AUTO_INCREMENT,
+nome_estufa VARCHAR(255),
+status_estufa TINYINT,
+fkempresa INT,
+CONSTRAINT chfkempresaestufa
+FOREIGN KEY (fkempresa) REFERENCES empresa (idempresa)
+);
+
+CREATE TABLE sensor(
+idsensor INT PRIMARY KEY AUTO_INCREMENT,
+nome_sensor VARCHAR(255),
+status_sensor TINYINT,
+dt_instalação DATETIME,
+dt_atualização DATETIME,
+fkestufa INT,
+CONSTRAINT chfkestufasensor 
+FOREIGN KEY (fkestufa) REFERENCES estufa (idestufa));
+
+CREATE TABLE leitura(
+idleitura INT PRIMARY KEY AUTO_INCREMENT,
+freq_luminosidade FLOAT,
+dt_capt_dados DATETIME,
+fksensor INT,
+CONSTRAINT chsensorleitura
+FOREIGN KEY (fksensor) REFERENCES sensor(idsensor));
+
+INSERT INTO empresa (razaosocial, cnpj, email, responsavel_legal, status_empresa) VALUES
+('Laboratório de Biologia Vegetal USP', '11111111000101', 'contato@lbvusp.br', 'Dra. Ana Ribeiro', 1),
+('Centro de Pesquisa Genética Unicamp', '22222222000102', 'suporte@geneticacamp.br', 'Dr. Lucas Martins', 1),
+('Embrapa Recursos Genéticos e Biotecnologia', '11111111000101', 'contato@embrapa.br', 'Dr. João Silva', 1),
+('Instituto Agronômico de Campinas (IAC)', '55555555000105', 'contato@iac.sp.gov.br', 'Dr. Rafael Costa', 1);
+
+INSERT INTO usuario (email, senha, cpf, fkempresa) VALUES
+('joao.silva@embrapa.br', 'embrapa123', '12345678901', 3),
+('ana.ribeiro@usp.br', 'usp123', '34567890123', 1),
+('carlos.lima@usp.br', 'usp456', '45678901234', 1),
+('lucas.martins@unicamp.br', 'unicamp123', '56789012345', 2),
+('maria.santos@iac.sp.gov.br', 'iac123', '89012345678', 4);
+
+INSERT INTO estufa (nome_estufa, status_estufa, fkempresa) VALUES
+('estufa EMBRAPA', 1, 3),
+('estufa USP', 1, 1),
+('estufa UNICAMP', 1, 2),
+('estufa IAC', 1, 4);
+
+INSERT INTO sensor (nome_sensor, status_sensor, dt_instalação, dt_atualização, fkestufa) VALUES
+('Sensor A - EMBRAPA', 1, '2026-03-01 08:00:00', '2026-04-01 09:00:00', 1),
+('Sensor B - USP', 1, '2026-03-02 09:00:00', '2026-04-01 09:20:00', 2),
+('Sensor C - UNICAMP', 1, '2026-03-03 10:00:00', '2026-04-01 09:40:00', 3),
+('sensor D - IAC', 1, '2026-03-05 12:00:00', '2026-04-01 10:10:00', 4);
+
+INSERT INTO leitura (freq_luminosidade, dt_capt_dados, fksensor) VALUES
+(110.5, '2026-04-01 09:00:00', 1),
+(120.0, '2026-04-01 10:00:00', 1),
+(130.2, '2026-04-01 11:00:00', 1),
+(150.0, '2026-04-01 09:00:00', 2),
+(160.4, '2026-04-01 10:00:00',2),
+(180.3, '2026-04-01 09:00:00', 3),
+(175.0, '2026-04-01 10:00:00', 3),
+(400.0, '2026-04-01 09:00:00', 4),
+(420.8, '2026-04-01 10:00:00', 4);
+
+
